b03b24ae96d1b9daa80d0a84bf239e9bd31e1bbc
yoneda hayato <yhayato1320@gmail.com>
2018-08-05 22:18:59 +0900

    added setting parameter

--- helper/setting_tmp.py
+++ helper/setting_tmp.py
@@ -3,3 +3,9 @@ import os
 # log
 root_path = "{}/".format(os.getcwd())
 log_save_path = root_path + "/helper/log"
+
+# stock data upload request url
+upload_request_url = ""
+
+# stock data download request url
+download_request_url = ""
