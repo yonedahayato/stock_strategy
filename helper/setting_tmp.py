966f191a3d9f8ddaef90a7ed00afc477627db31f
yonedahayato <yhayato1320@gmail.com>
2018-10-31 06:32:20 +0900

    added url to get code list

--- helper/setting_tmp.py
+++ helper/setting_tmp.py
@@ -7,3 +7,7 @@ log_save_path = root_path + "/helper/log"
 # stock data upload, download request url
 # request_url = "http://127.0.0.1:8090"
 request_url = "http://192.168.100.101:8090"
+
+# tosho_1st_list_url
+TOSHO_1ST_LIST_URL = "http://www.jpx.co.jp/markets/statistics-equities/misc/01.html"
+NIKKEI_225_LIST_URL = "https://indexes.nikkei.co.jp/nkave/index/component?idx=nk225"
