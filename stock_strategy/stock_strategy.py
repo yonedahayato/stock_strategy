a567db92dde17bc005407d59632dc09dffd90a11
yoneda hayato <yhayato1320@gmail.com>
2019-02-02 23:23:29 +0900

    修正

--- stock_strategy/stock_strategy.py
+++ stock_strategy/stock_strategy.py
@@ -71,7 +71,7 @@ class StockStrategy:
                 stock_data_df = stock_data_df.iloc[:-self.back_test_return_date]
 
         elif self.download_method == "LOCAL":
-            stock_data_df = pd.read_csv(HISTRICAL_DATA_PATH.format(code=code), header=None, index_col=0)
+            stock_data_df = pd.read_csv(HISTRICAL_DATA_PATH.format(code=code), index_col=0)
 
         if self.debug:
             print(msg.format(stock_data_df))
