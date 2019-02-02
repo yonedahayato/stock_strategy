06e3750755da63db7e8239aac98faa7986d9d88a
yoneda hayato <yhayato1320@gmail.com>
2019-02-02 22:57:09 +0900

    修正

--- stock_strategy/move_average_with_low_value.py
+++ stock_strategy/move_average_with_low_value.py
@@ -86,8 +86,8 @@ class MoveAverage(StockStrategy):
 
         sign_rising_Low = False
         for cnt in range(sign_rising_Low_term-1):
-            if (diff_Low_MoveAverage.iloc[cnt].values[0] == False) and \
-                (diff_Low_MoveAverage.iloc[cnt+1].values[0] == True):
+            if (diff_Low_MoveAverage.iloc[cnt] == False) and \
+                (diff_Low_MoveAverage.iloc[cnt+1] == True):
                 sign_rising_Low = True
                 break
 
