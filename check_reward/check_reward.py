6ab7a0892d06ff766e730d828c34814b38d2b906
yoneda hayato <yhayato1320@gmail.com>
2019-02-03 11:13:58 +0900

    check result のdocker 環境準備

--- check_reward/check_reward.py
+++ check_reward/check_reward.py
@@ -7,7 +7,6 @@ import sys
 import save_result
 from save_result import Save_Result
 sys.path.append(os.path.dirname(save_result.abspath))
-from move_average import Data_Downloader
 from check_list import *
 
 logger = save_result.logger
