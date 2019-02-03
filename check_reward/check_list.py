2d5944c1a2491e1b412b1b80392f0376c3f1847b
yoneda hayato <yhayato1320@gmail.com>
2019-02-03 11:29:58 +0900

    check result のdocker 環境準備

--- check_reward/check_list.py
+++ check_reward/check_list.py
@@ -1,6 +1,6 @@
 CHECK_LIST = [
-    "MoveAverageWithLowValue_window=75"
-    "MoveAverageWithLowValue_window=50"
-    "MoveAverageWithLowValue_window=25"
-    "MoveAverageWithLowValue_window=10"
+    "MoveAverageWithLowValue_window=75",
+    "MoveAverageWithLowValue_window=50",
+    "MoveAverageWithLowValue_window=25",
+    "MoveAverageWithLowValue_window=10",
 ]
