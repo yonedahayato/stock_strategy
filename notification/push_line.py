4adf792b106dc762245b5654956c30319457f1d0
yoneda hayato <yhayato1320@gmail.com>
2019-02-10 00:19:59 +0900

    push line image

--- notification/push_line.py
+++ notification/push_line.py
@@ -9,14 +9,18 @@ sys.path.append(p_dirname_push_line + "/helper")
 from setting import LINE_NOTIFY_TOKEN
 from log import logger
 
-def push_line(message):
+def push_line(message, image_path=None):
     logger.debug("[push line]: {}".format(message))
     line_notify_api = 'https://notify-api.line.me/api/notify'
 
     payload = {'message': message}
     headers = {'Authorization': 'Bearer ' + LINE_NOTIFY_TOKEN}  # 発行したトークン
-    files = {"imageFile": open("./notification/test_image.png", "rb")}
-    line_notify = requests.post(line_notify_api, data=payload, headers=headers, files=files)
+
+    if image_path == None:
+        line_notify = requests.post(line_notify_api, data=payload, headers=headers)
+    else:
+        files = {"imageFile": open(image_path, "rb")}
+        line_notify = requests.post(line_notify_api, data=payload, headers=headers, files=files)
 
 if __name__ == "__main__":
     push_line("test")
