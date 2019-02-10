54d3b8780fb1b8529a1874676b165ca500db1758
yoneda hayato <yhayato1320@gmail.com>
2019-02-10 14:03:46 +0900

    draw graph

--- draw_graph/draw_graph.py
+++ draw_graph/draw_graph.py
@@ -18,8 +18,11 @@ class DrawGraph:
         self.code = code
         self.method_name = method_name
 
+        self.graph_length = 100
+
     def draw(self):
         data_df = copy.deepcopy(self.data_df)
+        data_df = data_df.iat[-self.graph_length:]
         data_df.columns = ["Open", "High", "Low", "Close", "Adj_Close", "Volume"]
         data_df = data_df[["Open", "High", "Low", "Close", "Volume"]]
 
