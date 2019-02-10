9654ca26b0551db7a6f600b4a5cabd3b1e66191e
yoneda hayato <yhayato1320@gmail.com>
2019-02-10 22:42:18 +0900

    strategies に引数をついか

--- stock_strategy/strategies_back_test.sh
+++ stock_strategy/strategies_back_test.sh
@@ -1,2 +1,4 @@
-python stock_strategy/move_average_with_low_value.py
+$back_test_return_date=5
+
+python stock_strategy/move_average_with_low_value.py --back_test_return_date $back_test_return_date
 # python python stock_strategy/move_average.py
