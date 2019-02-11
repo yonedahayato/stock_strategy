fbfc3feb9fe922b8a6a779f478fbf952caf2b9a4
yoneda hayato <yhayato1320@gmail.com>
2019-02-11 12:21:44 +0900

    fix

--- stock_strategy/move_average.py
+++ stock_strategy/move_average.py
@@ -15,6 +15,7 @@ sys.path.append(p_path + "/get_stock_info")
 sys.path.append(p_path + "/get_stock_info/google_cloud_storage")
 sys.path.append(p_path + "/helper")
 sys.path.append(p_path + "/check_reward")
+sys.path.append(p_path + "/stock_strategy")
 
 from data_downloader import Data_Downloader
 from get_new_stock_code import GetCodeList
