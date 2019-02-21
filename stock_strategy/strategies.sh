55beba14c3fd6ea65c93c8b1019d78d9e279f5ca
yonedahayato <yhayato1320@gmail.com>
2019-02-21 21:41:48 +0900

    fix

--- stock_strategy/strategies.sh
+++ stock_strategy/strategies.sh
@@ -2,3 +2,4 @@ back_test_return_date=0
 
 python stock_strategy/move_average_with_low_value.py --back_test_return_date $back_test_return_date
 # python stock_strategy/move_average.py --back_test_return_date $back_test_return_date
+# python stock_strategy/trend_line.py --back_test_return_date $back_test_return_date
