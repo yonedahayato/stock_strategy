b536fec11006849b87ffee71718d971070c2d03a
yoneda <yoneda>
2019-05-19 14:48:50 +0900

    get_exchange_data作成

--- get_stock_info/get_exchange_data/get_exchange_data_from_oanda.py
+++ get_stock_info/get_exchange_data/get_exchange_data_from_oanda.py
@@ -1,17 +1,16 @@
 import copy
 import datetime
-import oandapy
+# import oandapy
 from oandapyV20 import API
 import oandapyV20.endpoints.instruments as oandapy
 import os
 from os import path
-import pandas as pd
-import pandas_datareader.data as web
 import sys
+import pandas as pd
 
 abs_dirname = path.dirname(path.abspath(__file__))
-parent_dirname = path.dirname(abs_dirname)
-helper_dirname = path.join(parent_dirname, "helper")
+home_dirname = path.dirname(path.dirname(abs_dirname))
+helper_dirname = path.join(home_dirname, "helper")
 sys.path.append(helper_dirname)
 
 from log import logger
@@ -25,42 +24,8 @@ from setting import (
     OANDA_API_KEY,
 )
 
-CURRENCY_FRED = {
-	'ドル/円': 'DEXJPUS',
-	'ユーロ/ドル': 'DEXUSEU',
-	'ポンド/ドル': 'DEXUSUK',
-    'ダウ平均': 'DJIA',
-    # 'DAX指数',
-    # ハンセン指数,
-    'SP500': 'SP500',
-    # 'NASDAQ',
-    # 'SHA',
-    }
 
-SOURCES = {
-    "fred": CURRENCY_FRED,
-}
-
-def make_save_data_dir():
-    save_dir = path.dirname(HISTRICAL_EXCHANGE_DATA_PATH)
-    os.makedirs(save_dir, exist_ok=True)
-
-
-def get_data(sources, currency, start, end):
-    logger.debug("source: {}".format(sources))
-    logger.debug("currency: {}".format(currency))
-
-    for source in sources.keys():
-        if source in ["fred"]:
-            data = web.DataReader(list(currency.values()), source, start, end)
-            data.columns = list(currency.keys())
-        elif sources:
-            get_data_from_oanda()
-
-    return data
-
-
-def get_data_from_oanda(instrument="USD_JPY"):
+def get_exchange_data_from_oanda(instrument="USD_JPY"):
     start = datetime.datetime(year=HISTRICAL_DATA_RANGE_START.year,
                               month=HISTRICAL_DATA_RANGE_START.month,
                               day=HISTRICAL_DATA_RANGE_START.day)
@@ -100,28 +65,5 @@ def get_data_from_oanda(instrument="USD_JPY"):
 
     return histrical_data
 
-def get_exchange_data(start=None, end=None, sources=None):
-    if start == None:
-        start = HISTRICAL_DATA_RANGE_START
-    if end == None:
-        end = HISTRICAL_DATA_RANGE_END_NOW
-    if sources == None:
-        sources = SOURCES
-
-    data_list = []
-    for source, currency in sources.items():
-        data_list.append(get_data(source, currency, start, end))
-
-    data = pd.concat(data_list, axis=0)
-
-    make_save_data_dir()
-    for source, currency in sources.items():
-        for key, name in currency.items():
-            data_for_save = data.loc[:, [key]]
-            data_for_save.columns = ["Close"]
-            data_for_save = data_for_save.fillna(method='bfill')
-            data_for_save.to_csv(HISTRICAL_EXCHANGE_DATA_PATH.format(name=name))
-
-if __name__ == '__main__':
-    # get_exchange_data()
-    get_data_from_oanda()
+if __name__ == "__main__":
+    get_exchange_data_from_oanda()
