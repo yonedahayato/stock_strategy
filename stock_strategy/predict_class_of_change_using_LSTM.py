c554574820c33d8085d40f72bc35d7364260af4d
yoneda <yoneda>
2019-06-22 20:08:11 +0900

    memory 対策

--- stock_strategy/predict_class_of_change_using_LSTM.py
+++ stock_strategy/predict_class_of_change_using_LSTM.py
@@ -97,7 +97,7 @@ class PredictClassOfChangeUsingLSTM(StockStrategy):
            just_pred_index in [2, 3]:
             self.result_codes.append(code)
 
-        del model
+        del model, stock_data_df, close_values, open_values, high_values, low_values
 
     def create_train_data(self, x_data, y_data, samples):
 
