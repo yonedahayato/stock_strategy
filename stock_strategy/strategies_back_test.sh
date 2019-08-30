2df00418430ad4343d1f793e5b5cb654b5ab25da
yoneda <yoneda>
2019-08-30 09:44:57 +0900

    strategies shell file 追加

--- stock_strategy/strategies_back_test.sh
+++ stock_strategy/strategies_back_test.sh
@@ -1,6 +1,7 @@
 back_test_return_date=5
 
 python stock_strategy/move_average_with_low_value.py --back_test_return_date $back_test_return_date
-# python stock_strategy/move_average.py --back_test_return_date $back_test_return_date
-# python stock_strategy/trend_line.py --back_test_return_date $back_test_return_date
-# python stock_strategy/trend_line_using_linear_regression.py --back_test_return_date $back_test_return_date
+python stock_strategy/move_average.py --back_test_return_date $back_test_return_date
+python stock_strategy/trend_line.py --back_test_return_date $back_test_return_date
+python stock_strategy/trend_line_using_linear_regression.py --back_test_return_date $back_test_return_date
+python stock_strategy/predict_class_of_change_using_LSTM.py --back_test_return_date $back_test_return_date
