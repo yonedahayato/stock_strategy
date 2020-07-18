38f0869444c08cc82e837f5a3276ca647fa21cd2
yoneda <yoneda>
2020-07-18 23:13:13 +0900

    jupyter 環境作成

--- Numerai/docker_exe.sh
+++ Numerai/docker_exe.sh
@@ -4,4 +4,5 @@ docker rmi ${IMAGE_NAME}
 docker build -t ${IMAGE_NAME} .
 docker run -it --rm \
            -v $PWD:/home/numerai \
+           -p 8888:8888 \
            ${IMAGE_NAME} /bin/bash
---
+++ Numerai/jupyter_run.sh
@@ -0,0 +1,1 @@
+nohup jupyter notebook --no-browser --ip=0.0.0.0 >> jupyter.log 2>&1 &
