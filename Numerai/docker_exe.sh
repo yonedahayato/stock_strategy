9ab9a3d3a025d713327fa84d8999bf4515715f08
yoneda <yoneda>
2020-07-19 16:07:22 +0900

    docker run するときのvolume 設定

--- Numerai/docker_exe.sh
+++ Numerai/docker_exe.sh
@@ -2,7 +2,10 @@ IMAGE_NAME=numerai
 
 docker rmi ${IMAGE_NAME}
 docker build -t ${IMAGE_NAME} .
+
+DATASET_DIR=$PWD/datasets
 docker run -it --rm \
            -v $PWD:/home/numerai \
+           -v ${DATASET_DIR}:/home/numerai/datasets \
            -p 8888:8888 \
            ${IMAGE_NAME} /bin/bash
--- Numerai/jupyter_run.sh
+++ Numerai/jupyter_run.sh
@@ -1,1 +1,2 @@
 nohup jupyter notebook --no-browser --ip=0.0.0.0 >> jupyter.log 2>&1 &
+cat jupyter.log | tail
