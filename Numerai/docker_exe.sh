ebdbecedf654abfcccc837b2c9513a9b1a123391
yoneda <yoneda>
2020-07-26 17:21:52 +0900

    docker_exe 修正

--- Numerai/docker_exe.sh
+++ Numerai/docker_exe.sh
@@ -3,9 +3,10 @@ IMAGE_NAME=numerai
 docker rmi ${IMAGE_NAME}
 docker build -t ${IMAGE_NAME} .
 
-DATASET_DIR=$PWD/datasets
+DATASET_DIR=$PWD/dataset
+HOST_JUPYTER_PORT=8888
 docker run -it --rm \
            -v $PWD:/home/numerai \
-           -v ${DATASET_DIR}:/home/numerai/datasets \
-           -p 8888:8888 \
+           -v ${DATASET_DIR}:/home/numerai/dataset \
+           -p HOST_JUPYTER_PORT:8888 \
            ${IMAGE_NAME} /bin/bash
