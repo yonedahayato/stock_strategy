c407466032a21ca136b7eb3f117e9eb1d9070a21
yoneda <yoneda>
2020-09-01 22:05:36 +0900

    docker_exe リファクタリング

--- docker_exe_select_stock_code.sh
+++ docker_exe_select_stock_code.sh
@@ -1,20 +1,20 @@
-# select stock code
+echo "select stock code"
 
-# docker build
+echo "docker build"
 # docker build -t select_stock_code_lib -f dockerfile/select_stock_code/Dockerfile_lib .
 docker build -t select_stock_code -f dockerfile/select_stock_code/Dockerfile .
 
-# log directory
+echo "make log directory"
 if [ ! -d "helper/log" ]; then
   mkdir helper/log
 fi
 
-# selected code directory
+echo "make selected code directory"
 if [! -d "check_reward/result/selected_code" ]; then
   mkdir check_reward/result/selected_code
 fi
 
-# environment
+echo "set environment"
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 DOWNLOAD_DIR=$SCRIPT_DIR/get_stock_info/stock_data
@@ -32,12 +32,13 @@ echo "LOG_DIR: " $LOG_DIR
 echo "SELECT_CODE_DIR: " $SELECT_CODE_DIR
 echo "GRAPH_DIR: " $GRAPH_DIR
 
-# get stock data
 export COMPOSE_FILE=dockerfile/docker-compose.select_stock_code.yml
+
+echo "docker compose up"
 # docker run -it --rm get_stock_data python get_stock_info/get_stock_data.py
 docker-compose up
 
 # remove docker image
 # docker rmi select_stock_code_lib
 docker-compose down -v
-docker rmi select_stock_code
+# docker rmi select_stock_code
