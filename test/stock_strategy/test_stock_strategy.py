f1ef63d530bfb57b83cd364f7693c52dd0002352
yoneda <yoneda>
2020-09-06 16:51:49 +0900

    test 追加

--- test/stock_strategy/test_stock_strategy.py
+++ test/stock_strategy/test_stock_strategy.py
@@ -14,7 +14,74 @@ from test_libs import (
 )
 
 class TestStockStrategy(object):
-    def test_shuld_execute_stock_strategy(self):
+    """TestStockStrategy class
+
+    stock strategy script のテスト
+
+    Attributes:
+        TEST_CODE(str): テストするコード番号
+
+    """
+    TEST_CODE = "1332"
+
+    def setup_method(self):
+        print("\nsetup now ...\n")
+        self.stock_strategy = StockStrategy(debug=True, back_test_return_date=5,
+                                            method_name="method_is_test")
+
+    def test_get_code_list(self):
+        """test_get_code_list func
+
+        code list の取得のテスト
+
+        """
+        code_list = self.stock_strategy.get_code_list()
+
+    def test_get_stock_data(self):
+        """test_get_stock_data func
+
+        ヒストリカルデータの取得のテスト
+
+        """
+        stock_data_df = self.stock_strategy.get_stock_data(self.TEST_CODE)
+
+    def test_select_code(self):
+        """test_select_code func
+
+        購買する銘柄かどうかを判断するテスト
+
+        """
+        stock_data_df = self.stock_strategy.get_stock_data(self.TEST_CODE)
+        self.stock_strategy.select_code(self.TEST_CODE, stock_data_df)
+
+    def test_save_result(self):
+        """test_save_result func
+
+        結果の保存のテスト
+
+        """
+        code_list = self.stock_strategy.get_code_list()
+        self.stock_strategy.result_codes = ["1332"]
+        self.stock_strategy.elapsed_times = [10]
+        json_result = self.stock_strategy.save_result()
+
+    def test_draw_graph(self):
+        """test_draw_graph func
+
+        グラフの描画のテスト
+
+        """
+        self.stock_strategy.result_codes = ["1332"]
+
+        # グラフ内容を確認したい場合はremove=Trueに
+        self.stock_strategy.draw_graph(remove=True)
+
+    def test_execute_stock_strategy(self):
+        """test_execute_stock_strategy func
+
+        stock_strategy の実行のテスト
+
+        """
         stock_strategy = StockStrategy(debug=True, back_test_return_date=5,
                                        method_name="method_is_test")
         stock_strategy.execute()
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,3 @@
-pytest -v check_reward/test_result.py
-pytest -v get_stock_info/google_cloud_storage/*
-pytest -v stock_strategy/test_stock_strategy.py
+# pytest -v check_reward/test_result.py
+# pytest -v get_stock_info/google_cloud_storage/*
+python -m pytest -v stock_strategy/test_stock_strategy.py -s
