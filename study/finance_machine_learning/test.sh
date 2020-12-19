4cdbd677b1990a42cac4339ba854cf2225b823e4
yoneda <yoneda>
2020-12-19 15:43:22 +0900

    test script の調整

--- study/finance_machine_learning/test.sh
+++ study/finance_machine_learning/test.sh
@@ -1,1 +1,2 @@
-python -m pytest -v test.py -s
+# python -m pytest -v test.py::TestFeatureImportance -s --disable-warnings
+python -m pytest -v test.py::TestOriginalData::test_original_dataset -s --disable-warnings
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
 # pytest -v check_reward/test_result.py
 # pytest -v get_stock_info/google_cloud_storage/*
-python -m pytest -v stock_strategy/test_stock_strategy.py -s
+# python -m pytest -v stock_strategy/test_stock_strategy.py -s
+python -m pytest -v get_stock_info/test.py::TestGetRowData -s --disable-warnings
